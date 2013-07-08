a071ab22d6998e52e010017fa948e3e7620f24da
fisherman77 <fisherman77mc@gmail.com>
2013-07-08 10:40:37 -0500

    First Commit
    Laying the railroad for the mod

---
+++ TutorialMod/client/TutorialModClientProxy.java
@@ -0,0 +1,11 @@
+package fisherman77.TutorialMod.client;
+
+import fisherman77.TutorialMod.common.TutorialModCommonProxy;
+import net.minecraftforge.client.MinecraftForgeClient;
+
+public class TutorialModClientProxy extends TutorialModCommonProxy {
+        
+public void registerRenderInformation(){
+}
+
+}
