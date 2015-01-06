ea552f89455f37093cf86311ce29a1f0033b8c59
Dave Welch <david-welch@uiowa.edu>
2015-01-05 22:58:31 -0600

    WIP: cement bucket and limestone

--- src/main/java/org/coderdojoia/tutorialmod/TutorialMod.java
+++ src/main/java/org/coderdojoia/tutorialmod/TutorialMod.java
@@ -1,10 +1,13 @@
 package org.coderdojoia.tutorialmod;
 
-// Make the needed the libraries accessible
 import net.minecraft.block.Block;
+import net.minecraft.block.material.MapColor;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.material.MaterialLiquid;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
@@ -13,43 +16,37 @@ import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.registry.GameRegistry;
 import org.coderdojoia.tutorialmod.BlockConcrete;
+import org.coderdojoia.tutorialmod.Cement;
 
-@Mod(modid = TutorialMod.MODID, version = TutorialMod.VERSION, name="tutorialmod") // Forge annotation
-public class TutorialMod  // <-- mod class
+@Mod(modid = TutorialMod.MODID, version = TutorialMod.VERSION, name="tutorialmod")
+public class TutorialMod
 {
-	/* 
-	 * Make MODID and VERSION fields:
-	 *  - usable everywhere (with 'public')
-	 *  - usable without a specific TutorialMod() created (with 'static')
-	 *  - unchangeable (with 'final')
-	 *  - a String type (instead of an integer, float, char, etc.)
-	 * 
-	 *  *!*!* By convention, 'final' fields are in ALL CAPS *!*!*
-	 */
     public static final String MODID = "tutorialmod";
     public static final String VERSION = "1.0";
-    
     public static Block concrete;
-    /* Forge mods have 3-stages of initialization:
-     * - preInit: register your mods basics
-     * - init: mod-specific code
-     * - postInit: how your mod works with other mods
-     */
+    public static Block limestone;
+    public static MaterialLiquid cement;
+    public static Item cement_bucket;
+    
     @EventHandler
     public void preInit(FMLPreInitializationEvent event)
     {
     	concrete = new BlockConcrete();
     	GameRegistry.registerBlock(concrete, "Concrete");
+    	limestone = new BlockLimestone();
+    	GameRegistry.registerBlock(limestone, "Limestone");
+    	cement_bucket = new ItemCement_Bucket();
+    	GameRegistry.registerItem(cement_bucket, "Cement Bucket");
+    	
     }
     
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
     	/*
-		 * We can't test concrete's harvestLevel in Creative mode,
-		 * so we need a temporary recipe to create concrete in Survival
-		 *      2 cobblestone = 4 concrete
-		 */
+    	 * Let's create some recipes for concrete:
+    	 *   1) a 'shapeless' recipe of  
+    	 */
     	ItemStack shapelessInput = new ItemStack(Blocks.cobblestone, 2);
     	ItemStack shapelessOutput = new ItemStack(concrete, 4);
         GameRegistry.addShapelessRecipe(shapelessOutput, shapelessInput);
